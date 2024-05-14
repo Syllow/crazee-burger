17f0c23b832c748dedfc79254edb7ea430f11641
Vincent <vince.derrien@gmail.com>
2024-05-14 17:40:22 +0000

    ✨ F01: LOGIN > Add Titles and Form

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,3 +1,33 @@
+import { useState } from 'react';
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue('');
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action='submit' onSubmit={handleSubmit}>
+        <input
+          value={inputValue}
+          type='text'
+          placeholder='Entrez votre prénom...'
+          onChange={handleChange}
+          required
+        />
+        <button>Accéder à votre espace</button>
+      </form>
+    </div>
+  );
 }
